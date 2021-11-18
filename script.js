ab60f78eea6b9493c473120841b539072a62e396
Lukasz Kot <lukasz.kot@hotmail.com>
2021-11-18 11:34:31 +0100

    Code cleanup.

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
 
-const test = document.querySelector('.test');
-const test1 = document.querySelector('.test');
-const test2 = document.querySelector('.test');
-const test3 = document.querySelector('.test');
-const test4 = document.querySelector('.test');
-const test5 = document.querySelector('.test');
-
 const cityName = document.querySelector('.city-name');
 const currentTemp = document.querySelector('.current-temp');
 const currentDay = document.querySelector('.current-day');
@@ -17,7 +10,7 @@ const warning = document.querySelector('.warning');
 const weatherIconCurrent = document.querySelector('.weather-icon-current');
 const weatherIconOne = document.querySelector('.weather-icon-one');
 const weatherIconTwo = document.querySelector('.weather-icon-two');
-const weatherIconThree = document.querySelector('.weather-icon-three');
+// const weatherIconThree = document.querySelector('.weather-icon-three');
 const weatherIconFour = document.querySelector('.weather-icon-four');
 const weatherIconFive = document.querySelector('.weather-icon-five');
 const weatherIconSix = document.querySelector('.weather-icon-six');
@@ -34,7 +27,7 @@ const sunset = document.querySelector('.sunset');
 // Forecast for the next 6 days
 const dayOne = document.querySelector('.day-one');
 const dayTwo = document.querySelector('.day-two');
-const dayThree = document.querySelector('.day-three');
+// const dayThree = document.querySelector('.day-three');
 const dayFour = document.querySelector('.day-four');
 const dayFive = document.querySelector('.day-five');
 const daySix = document.querySelector('.day-six');
@@ -42,7 +35,7 @@ const daySeven = document.querySelector('.day-seven');
 
 const tempMaxOne = document.querySelector('.temp-max-one');
 const tempMaxTwo = document.querySelector('.temp-max-two');
-const tempMaxThree = document.querySelector('.temp-max-three');
+// const tempMaxThree = document.querySelector('.temp-max-three');
 const tempMaxFour = document.querySelector('.temp-max-four');
 const tempMaxFive = document.querySelector('.temp-max-five');
 const tempMaxSix = document.querySelector('.temp-max-six');
@@ -50,7 +43,7 @@ const tempMaxSeven = document.querySelector('.temp-max-seven');
 
 const tempMinOne = document.querySelector('.temp-min-one');
 const tempMinTwo = document.querySelector('.temp-min-two');
-const tempMinThree = document.querySelector('.temp-min-three');
+// const tempMinThree = document.querySelector('.temp-min-three');
 const tempMinFour = document.querySelector('.temp-min-four');
 const tempMinFive = document.querySelector('.temp-min-five');
 const tempMinSix = document.querySelector('.temp-min-six');
@@ -69,22 +62,17 @@ const units = '&units=metric';
 let city;
 let url;
 let urlNew;
-// let wind;
 
 const getWeather = () => {
 	city = !input.value ? 'Warsaw' : input.value;
-
-	// url = apiLink + city + apiKey + units;
 	url = apiLink + city + apiKey + units;
 
 	// przy pomocy axios'a pobieramy url, który u góry stworzyliśmy
-
 	axios
 		.get(url)
 		.then((res) => {
 			// res.data.list.forEach(day => console.log(day.main.temp))
 			console.log(res);
-
 
 			const temp = res.data.main.temp;
 			const wind = res.data.wind.speed;
@@ -93,18 +81,11 @@ const getWeather = () => {
 			const hum = res.data.main.humidity;
 			const unixSunrise = res.data.sys.sunrise;
 			const unixSunset = res.data.sys.sunset;
-			// console.log(wind);
-			// console.log(res.data.main.humidity);
-			// console.log(res.data);
+
 			feelsLike.textContent = Math.floor(feels) + '°C';
 			windSpeed.textContent = wind + ' km/h';
 			pressure.textContent = press + ' hPa';
 			humidity.textContent = hum + ' %';
-
-			// let unixDate = new Date(data.daily[0].dt * 1000);
-			// console.log(unixDate);
-			// let hours = unixDate.getHours();
-			// console.log(hours);
 
 			// Sunrise
 			const unixTimeSunrise = new Date(unixSunrise * 1000);
@@ -117,53 +98,17 @@ const getWeather = () => {
 			const minutesSunset = unixTimeSunset.getMinutes() < 10 ? '0' + unixTimeSunset.getMinutes() : unixTimeSunset.getMinutes();
 			sunset.textContent = hoursSunset + ':' + minutesSunset;
 
-			// const test = res.data.list[5].main.humidity;
-			// czyli do status przypiszemy zwykły pojedynczy obiekt - przy użyciu assign'a "rozsmarujemy" całą tablicę, która znajduje się w res.data.weather
+			// Do status przypiszemy zwykły pojedynczy obiekt - przy użyciu assign'a "rozsmarujemy" całą tablicę, która znajduje się w res.data.weather
 			const status = Object.assign({}, ...res.data.weather);
-			// console.log(status);
-			// console.log(status.id);
-			// console.log(wind);
 
 			cityName.textContent = res.data.name;
 			currentTemp.textContent = Math.floor(temp) + '°C';
-			// console.log(wind);
-			// windSpeed.textContent = wind + 'km/h';
-			// feelsLike.textContent = Math.floor(feels) + '°C';
-			// humidity.textContent = hum + '%';
-			// weather.textContent = status.main;
-			// console.log(status.id);
-
 			warning.textContent = '';
 			input.value = '';
 
-			// console.log(status.main);
-
-			// console.log(status.description);
-
-			// whatWeather(weatherIconCurrent, status.id);
 			whatWeather(weatherIconCurrent, status.id);
 
-			// if (status.id >= 200 && status.id < 300) {
-			// 	weatherIcon.setAttribute('src', './img/thunderstorm.svg');
-			// } else if (status.id >= 300 && status.id < 400) {
-			// 	weatherIcon.setAttribute('src', './img/drizzle.svg');
-			// } else if (status.id >= 500 && status.id < 600) {
-			// 	weatherIcon.setAttribute('src', './img/rain.svg');
-			// } else if (status.id >= 600 && status.id < 700) {
-			// 	weatherIcon.setAttribute('src', './img/snow.svg');
-			// } else if (status.id === 701) {
-			// 	weatherIcon.setAttribute('src', './img/fog.svg');
-			// } else if (status.id === 800) {
-			// 	weatherIcon.setAttribute('src', './img/sun.svg');
-			// } else if (status.id >= 801 && status.id <= 804) {
-			// 	weatherIcon.setAttribute('src', './img/cloud.svg');
-			// } else {
-			// 	weatherIcon.setAttribute('src', './img/unknown.svg');
-			// }
-			// Poniżej dzięki temu w konsoli można zobaczyć jaki obiekt nam się pobrał
-			// console.log(res);
-
-			// Link to the city by coordinates from the inserted location.
+			// Second API link to get the city by coordinates from the firstly inserted location.
 			urlNew =
 				apiLinkSecond +
 				res.data.coord.lat +
@@ -172,11 +117,9 @@ const getWeather = () => {
 				'&exclude=minutely,hourly' +
 				apiKey +
 				units;
-			// console.log(urlNew);
 
 			getForecast(urlNew);
 
-			// console.log(urlSecond);
 		})
 		.catch(() => {
 			warning.textContent = 'Enter the correct city name.';
@@ -189,7 +132,7 @@ const enterCheck = () => {
 	}
 };
 
-// Test function to assign a weather icon.
+// Function to assign a weather icon.
 const whatWeather = (dayIcon, dayID) => {
 	let weatherIcon = dayIcon;
 
@@ -218,20 +161,7 @@ const getForecast = (urlSecond) => {
 		.then((res) => res.json())
 		.then((data) => {
 			console.log(data);
-			// status = Object.assign({}, ...res.data.daily);
-			// console.log(data.daily[6].temp.day);
-			// console.log(data);
 
-			// data.daily.forEach(console.log(data.temp))
-
-			// for (const day of data.daily){
-			// 	console.log(day.temp);
-			// }
-			// console.log(data.daily[2].temp);
-
-			// tempOne.textContent = data.daily[1].temp.day;
-			// tempTwo.textContent = data.daily[2].temp.day;
-			// tempThree.textContent = data.daily[3].temp.day;
 			const days = [
 				'Sunday',
 				'Monday',
@@ -319,7 +249,7 @@ const getForecast = (urlSecond) => {
 			tempMaxSix.textContent = Math.floor(data.daily[dayIndex].temp.max) + '°C';
 			tempMinSix.textContent = Math.floor(data.daily[dayIndex].temp.min) + '°C';
 
-			// Seventh day
+			// Seventh day (but third day, "long story...")
 			d = new Date(data.daily[dayIndex].dt * 1000);
 			dayName = days[d.getDay()];
 			daySeven.textContent = dayName;
